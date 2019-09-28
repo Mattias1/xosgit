3514e56b96076e657d5a2d1b11bdd11c1d85817f
Mattias <1116150+Mattias1@users.noreply.github.com>
2019-09-28 10:58:19 +0200

    Don't crash if git complains about one repo

--- src/crossosgit.py
+++ src/crossosgit.py
@@ -2,17 +2,21 @@ import git
 import repogroup
 from pathlib import Path
 
+
 class CrossOsGit:
     def __init__(self, configFile: Path):
         self.group = self.loadRepoGroup(configFile)
 
     def update(self):
         for repo in self.group.repos:
-            path = self.group.destinationDir / repo.name
-            if path.exists():
-                self.pull(repo)
-            else:
-                self.clone(repo)
+            try:
+                path = self.group.destinationDir / repo.name
+                if path.exists():
+                    self.pull(repo)
+                else:
+                    self.clone(repo)
+            except git.GitCommandError as e:
+                print(e)
 
     def clone(self, repo: repogroup.GitRepo):
         destinationPath = self.group.destinationDir / repo.name
